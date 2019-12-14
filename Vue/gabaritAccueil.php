57311073ae0f14556fd553f1570d421a1cc8eff9
MartinsRomain <romain.martins@yahoo.fr>
2019-12-14 23:25:24 +0100

    titre + submit sur pageAccueil

--- Vue/gabaritAccueil.php
+++ Vue/gabaritAccueil.php
@@ -9,12 +9,17 @@
 </head>
 
 <body>
+	<h1>QUI ES TU ETRANGER</h1>
 	<form action="gabaritAccueil" method="post">
-		<label>Login :</label><input type="text" name="login" /><br/>
-		<label>Mot de passe :</label><input type="password" name="mdp"/><br/>
-		<input type="radio" name="grade" value="Agent" checked><label>Agent</label><br/>
-		<input type="radio" name="grade" value="Medecin"><label>Médecin</label><br/>
-		<input type="radio" name="grade" value="Directeur"><label>Directeur</label><br/>
+		<fieldset>
+			<legend>Connexion</legend>
+			<label>Login :</label><input type="text" name="login" /><br/>
+			<label>Mot de passe :</label><input type="password" name="mdp"/><br/>
+			<input type="radio" name="grade" value="Agent" checked><label>Agent</label><br/>
+			<input type="radio" name="grade" value="Medecin"><label>Médecin</label><br/>
+			<input type="radio" name="grade" value="Directeur"><label>Directeur</label><br/>
+			<input type="submit" name="seConnecter" value="Connexion" />
+		</fieldset>
 	</form>
 
 
