e9e01c7120482c602f6db85932cbaedf11a3293b
Maxence Alvarez <31598428+maxencealvarez@users.noreply.github.com>
2019-12-15 00:08:04 +0100

    Ajout bouton
    Ajout bouton "Ajouter créneau"

--- Vue/script_medecin.js
+++ Vue/script_medecin.js
@@ -5,5 +5,6 @@ function ajouter_zone() {
 	for (i = 0; i<nb_creneau; i++){
 		chaine = chaine + '</br><label>Créneau '+eval(i+1)+' : </label><input type="text" name="'+i+'"></br>';
 	}
+	chaine = chaine + '</br><input type="submit" name="ajouter_creneau" value="Ajouter créneaux">';
 	document.getElementById('ajouter').innerHTML=chaine;
 }
