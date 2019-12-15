8cd0921527e38a290671718b393b2ea6a17e596d
Maxence Alvarez <31598428+maxencealvarez@users.noreply.github.com>
2019-12-15 15:54:21 +0100

    Leger Ajouts/Modifications
    - Ajout du code de connection à la bdd dans connect.php
    
    -Ajout fonction getConnect() dans modele.php
    
    - Amelioration de la page medecin (Gabarit + script js)

--- Modele/connect.php.php
+++ Modele/connect.php.php
@@ -1,1 +1,7 @@
 <?php
+define("SERVEUR","localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("BDD", "NOM_BDD");
+
+// a Modifier en fonction de la bdd
--- Vue/script_medecin.js
+++ Vue/script_medecin.js
@@ -1,3 +1,7 @@
+
+
+// fonction affiche zones de texte en fonction du nombre de créneaux choisi
+
 function ajouter_zone() {
 	var nb_creneau = window.document.forms[0].elements['nb_creneau'].value;
 	var chaine = "";
@@ -5,6 +9,12 @@ function ajouter_zone() {
 	for (i = 0; i<nb_creneau; i++){
 		chaine = chaine + '</br><label>Créneau '+eval(i+1)+' : </label><input type="text" name="'+i+'"></br>';
 	}
-	chaine = chaine + '</br><input type="submit" name="ajouter_creneau" value="Ajouter créneaux">';
+	if (nb_creneau==0 || nb_creneau<0 || isNaN(nb_creneau)){
+		chaine = chaine;
+	}
+	else{
+		chaine = chaine + '</br><input type="submit" name="ajouter_creneau" value="Ajouter créneaux">';
+	}
+	
 	document.getElementById('ajouter').innerHTML=chaine;
 }
